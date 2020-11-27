2cf4843ce0fc3bae523596ed516164f711a500b9
Son Le <sonle.itsme@gmail.com>
2020-11-27 20:07:19 +0700

    Yahoo pull campaign

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -417,10 +417,6 @@ class CampaignController extends Controller
 
     public function media()
     {
-        PullCampaign::dispatch(auth()->user());
-
-        return;
-
         $adVendorClass = 'App\\Utils\\AdVendors\\' . ucfirst(request('provider'));
 
         return (new $adVendorClass)->media();
