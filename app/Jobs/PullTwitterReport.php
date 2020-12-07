6c3f3711082264dbe27a1ceb37c19c4fbaaf9de4
Son Le <sonle.itsme@gmail.com>
2020-12-07 17:12:37 +0700

    Fix

--- app/Jobs/PullTwitterReport.php
+++ app/Jobs/PullTwitterReport.php
@@ -39,38 +39,34 @@ class PullTwitterReport implements ShouldQueue
      */
     public function handle()
     {
-        try {
-            $current_date = new DateTime;
-            $next_date = (new DateTime)->add(new DateInterval('P1D'));
+        $current_date = new DateTime;
+        $next_date = (new DateTime)->add(new DateInterval('P1D'));
 
-            foreach (TwitterReport::METRIC_GROUPS as $metric_group) {
-                foreach (TwitterReport::PLACEMENTS as $placement) {
-                    $api = new TwitterAPI(UserProvider::where('provider_id', $this->campaign->provider_id)->where('open_id', $this->campaign->open_id)->first(), $this->campaign->advertiser_id);
+        foreach (TwitterReport::METRIC_GROUPS as $metric_group) {
+            foreach (TwitterReport::PLACEMENTS as $placement) {
+                $api = new TwitterAPI(UserProvider::where('provider_id', $this->campaign->provider_id)->where('open_id', $this->campaign->open_id)->first(), $this->campaign->advertiser_id);
 
-                    $report = TwitterReport::firstOrNew([
-                        'metric_groups' => $metric_group,
-                        'campaign_id' => $this->campaign->id,
-                        'granularity' => 'DAY',
-                        'placement' => $placement,
-                        'start_time' => $current_date->format('Y-m-d'),
-                        'end_time' => $next_date->format('Y-m-d')
-                    ]);
+                $report = TwitterReport::firstOrNew([
+                    'metric_groups' => $metric_group,
+                    'campaign_id' => $this->campaign->id,
+                    'granularity' => 'DAY',
+                    'placement' => $placement,
+                    'start_time' => $current_date->format('Y-m-d'),
+                    'end_time' => $next_date->format('Y-m-d')
+                ]);
 
-                    $report->data = json_encode($api->getCampaignStats($this->campaign->campaign_id, [
-                        'metric_groups' => $metric_group,
-                        'entity' => 'CAMPAIGN',
-                        'entity_ids' => $this->campaign->campaign_id,
-                        'granularity' => 'DAY',
-                        'placement' => $placement,
-                        'start_time' => $current_date->format('Y-m-d'),
-                        'end_time' => $next_date->format('Y-m-d')
-                    ]));
+                $report->data = json_encode($api->getCampaignStats($this->campaign->campaign_id, [
+                    'metric_groups' => $metric_group,
+                    'entity' => 'CAMPAIGN',
+                    'entity_ids' => $this->campaign->campaign_id,
+                    'granularity' => 'DAY',
+                    'placement' => $placement,
+                    'start_time' => $current_date->format('Y-m-d'),
+                    'end_time' => $next_date->format('Y-m-d')
+                ]));
 
-                    $report->save();
-                }
+                $report->save();
             }
-        } catch(Exception $e) {
-
         }
     }
 }
