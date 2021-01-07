bc64106657f82078ebcaeb0774f3c94c05563090
Nam Huỳnh <namhuynh@MacBook-Pro-cua-Nam.local>
2021-01-07 18:21:37 +0700

    Won't seed anymore

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,10 +15,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        $this->call(ProvidersTableSeeder::class);
-        $this->call(TrackersTableSeeder::class);
-        $this->call(UsersTableSeeder::class);
-        $this->call(RulesTableSeeder::class);
+        // $this->call(ProvidersTableSeeder::class);
+        // $this->call(TrackersTableSeeder::class);
+        // $this->call(UsersTableSeeder::class);
+        // $this->call(RulesTableSeeder::class);
         $this->call(NetworkSettingGroupSeeder::class);
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
