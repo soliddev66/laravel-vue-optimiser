bb43d52ffc696fe864b02701cc8ff58edfd1be91
Tình Phong <tinhphong007@gmail.com>
2021-01-21 15:08:37 +0700

    Yahoo Japan: Campaign creator: Fix disable button

--- resources/js/components/ad-vendors/YahooJP.vue
+++ resources/js/components/ad-vendors/YahooJP.vue
@@ -554,7 +554,6 @@ export default {
       this.$modal.show('imageModal')
     },
     validURL(str) {
-      console.log(str)
       var pattern = /^(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g;
       return !!pattern.test(str);
     },
