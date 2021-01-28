49341b478389d6b8e16354973461afadd65b4ca3
Tình Phong <tinhphong007@gmail.com>
2021-01-28 14:35:32 +0700

    Job: Improvide job priorities

--- app/Jobs/PullTaboolaReport.php
+++ app/Jobs/PullTaboolaReport.php
@@ -22,15 +22,14 @@ class PullTaboolaReport implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    protected $campaigns;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct($campaigns)
+    public function __construct()
     {
-        $this->campaigns = $campaigns;
+
     }
 
     /**
@@ -42,31 +41,33 @@ class PullTaboolaReport implements ShouldQueue
     {
         $current_date = new DateTime;
 
-        foreach ($this->campaigns as $campaign) {
-            $api = new TaboolaAPI(UserProvider::where([
-                'provider_id' => $campaign->provider_id,
-                'open_id' => $campaign->open_id
-            ])->first());
+        Campaign::where('provider_id', 4)->chunk(10, function ($campaigns) use ($current_date) {
+            foreach ($campaigns as $campaign) {
+                $api = new TaboolaAPI(UserProvider::where([
+                    'provider_id' => $campaign->provider_id,
+                    'open_id' => $campaign->open_id
+                ])->first());
 
-            if ($api) {
-                $reports = $api->getReport($campaign->advertiser_id, $campaign->campaign_id, $current_date->format('Y-m-d'), $current_date->format('Y-m-d'))['results'];
-                if (count($reports) > 0) {
-                    foreach ($reports as $item) {
-                        $report = TaboolaReport::firstOrNew([
-                            'campaign_id' => $campaign->id,
-                            'date' => $current_date->format('Y-m-d')
-                        ]);
+                if ($api) {
+                    $reports = $api->getReport($campaign->advertiser_id, $campaign->campaign_id, $current_date->format('Y-m-d'), $current_date->format('Y-m-d'))['results'];
+                    if (count($reports) > 0) {
+                        foreach ($reports as $item) {
+                            $report = TaboolaReport::firstOrNew([
+                                'campaign_id' => $campaign->id,
+                                'date' => $current_date->format('Y-m-d')
+                            ]);
 
-                        unset($item['date']);
+                            unset($item['date']);
 
-                        foreach (array_keys($item) as $key) {
-                            $report->{$key} = $item[$key];
+                            foreach (array_keys($item) as $key) {
+                                $report->{$key} = $item[$key];
+                            }
+
+                            $report->save();
                         }
-
-                        $report->save();
                     }
                 }
             }
-        }
+        });
     }
 }
