12751875f577f4dcdcf8aed02def3958d6981380
Tình Phong <tinhphong007@gmail.com>
2021-01-28 17:43:57 +0700

    Job: Implement job priorities

--- app/Jobs/PullTwitterReport.php
+++ app/Jobs/PullTwitterReport.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use DB;
 use DateTime;
 use Exception;
 use DateInterval;
@@ -22,17 +23,14 @@ class PullTwitterReport implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    protected $campaigns;
-    protected $campaign_account;
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct($campaigns, $campaign_account)
+    public function __construct()
     {
-        $this->campaigns = $campaigns;
-        $this->campaign_account = $campaign_account;
+
     }
 
     /**
@@ -45,44 +43,59 @@ class PullTwitterReport implements ShouldQueue
         $current_date = new DateTime;
         $next_date = (new DateTime)->add(new DateInterval('P1D'));
 
-        $campaign_ids = [];
+        DB::table('user_providers')->where('provider_id', 3)->chunkById(5, function ($user_providers) use ($current_date, $next_date) {
+            foreach ($user_providers as $user_provider) {
+                $campaign_accounts = DB::table('campaigns')->select('open_id', 'provider_id', 'advertiser_id')->groupBy('open_id', 'provider_id', 'advertiser_id')->where([
+                    'provider_id' => $user_provider->provider_id,
+                    'open_id' => $user_provider->open_id
+                ])->get();
 
-        foreach ($this->campaigns as $campaign) {
-            $campaign_ids[] = $campaign->campaign_id;
-        }
+                foreach ($campaign_accounts as $campaign_account) {
+                    DB::table('campaigns')->where(['advertiser_id' => $campaign_account->advertiser_id])->chunkById(20, function ($campaigns) use ($campaign_account, $current_date, $next_date) {
+                        $campaign_ids = [];
 
-        $api = new TwitterAPI(UserProvider::where([
-            'provider_id' => $this->campaign_account->provider_id,
-            'open_id' => $this->campaign_account->open_id
-        ])->first(), $this->campaign_account->advertiser_id);
+                        foreach ($campaigns as $campaign) {
+                            $campaign_ids[] = $campaign->campaign_id;
+                        }
 
-        foreach (TwitterReport::PLACEMENTS as $placement) {
-            $report_datas = $api->getCampaignStats([
-                'metric_groups' => implode(',', TwitterReport::METRIC_GROUPS),
-                'entity' => 'CAMPAIGN',
-                'entity_ids' => implode(',', $campaign_ids),
-                'granularity' => 'DAY',
-                'placement' => $placement,
-                'start_time' => $current_date->format('Y-m-d'),
-                'end_time' => $next_date->format('Y-m-d')
-            ]);
+                        $api = new TwitterAPI(UserProvider::where([
+                            'provider_id' => $campaign_account->provider_id,
+                            'open_id' => $campaign_account->open_id
+                        ])->first(), $campaign_account->advertiser_id);
 
-            foreach ($report_datas as $report_data) {
-                $campaign = Campaign::where('campaign_id', $report_data->id)->first();
-                if ($campaign) {
-                    $report = TwitterReport::firstOrNew([
-                        'campaign_id' => $campaign->id,
-                        'granularity' => 'DAY',
-                        'placement' => $placement,
-                        'start_time' => $current_date->format('Y-m-d'),
-                        'end_time' => $next_date->format('Y-m-d')
-                    ]);
+                        foreach (TwitterReport::PLACEMENTS as $placement) {
+                            $report_datas = $api->getCampaignStats([
+                                'metric_groups' => implode(',', TwitterReport::METRIC_GROUPS),
+                                'entity' => 'CAMPAIGN',
+                                'entity_ids' => implode(',', $campaign_ids),
+                                'granularity' => 'DAY',
+                                'placement' => $placement,
+                                'start_time' => $current_date->format('Y-m-d'),
+                                'end_time' => $next_date->format('Y-m-d')
+                            ]);
 
-                    $report->data = json_encode($report_data->id_data);
+                            foreach ($report_datas as $report_data) {
+                                $campaign = Campaign::where('campaign_id', $report_data->id)->first();
+                                if ($campaign) {
+                                    $report = TwitterReport::firstOrNew([
+                                        'campaign_id' => $campaign->id,
+                                        'granularity' => 'DAY',
+                                        'placement' => $placement,
+                                        'start_time' => $current_date->format('Y-m-d'),
+                                        'end_time' => $next_date->format('Y-m-d')
+                                    ]);
 
-                    $report->save();
+                                    $report->data = json_encode($report_data->id_data);
+
+                                    $report->save();
+                                }
+                            }
+                        }
+
+                        sleep(10);
+                    });
                 }
             }
-        }
+        });
     }
 }
--- app/Vngodev/Twitter.php
+++ app/Vngodev/Twitter.php
@@ -3,7 +3,6 @@
 namespace App\Vngodev;
 
 use App\Jobs\PullTwitterReport;
-use DB;
 
 class Twitter
 {
@@ -11,22 +10,6 @@ class Twitter
 
     public static function getReport()
     {
-        DB::table('user_providers')->where('provider_id', 3)->chunkById(5, function ($user_providers) {
-            foreach ($user_providers as $user_provider) {
-                $campaign_accounts = DB::table('campaigns')->select('open_id', 'provider_id', 'advertiser_id')->groupBy('open_id', 'provider_id', 'advertiser_id')->where([
-                    'provider_id' => $user_provider->provider_id,
-                    'open_id' => $user_provider->open_id
-                ])->get();
-
-                foreach ($campaign_accounts as $campaign_account) {
-                    DB::table('campaigns')->where([
-                        'advertiser_id' => $campaign_account->advertiser_id
-                    ])->chunkById(20, function ($campaigns) use ($campaign_account) {
-                        PullTwitterReport::dispatch($campaigns, $campaign_account);
-                        sleep(10);
-                    });
-                }
-            }
-        });
+        PullTwitterReport::dispatch();
     }
 }
