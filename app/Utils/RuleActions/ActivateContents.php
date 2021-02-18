753571c16e7811d9915faf3deb0a1fe4746a1acb
Tình Phong <tinhphong007@gmail.com>
2021-02-18 17:38:54 +0700

    Rule: Notification: Visual

--- app/Utils/RuleActions/ActivateContents.php
+++ app/Utils/RuleActions/ActivateContents.php
@@ -9,15 +9,30 @@ use App\Endpoints\GeminiAPI;
 
 class ActivateContents extends Root
 {
-    public function process($campaign, $ad)
+    public function process($campaign, $ad, &$log)
     {
+        $log['effect'] = [
+            'campaign' => $campaign->name,
+            'ad_group_id' => $ad->ad_group_id,
+            'ad_id' => $ad->ad_id
+        ];
         try {
             $adVendorClass = 'App\\Utils\\AdVendors\\' . ucfirst($campaign->provider->slug);
 
             (new $adVendorClass)->adStatus($campaign, $ad->ad_group_id, $ad->ad_id, Campaign::STATUS_ACTIVE);
+            $log['effect']['activated'] = true;
             echo 'Ad was being activated', "\n";
         } catch (Exception $e) {
             echo "Ad wasn't being activated\n";
         }
+    }
+
+    public function visual($campaign, $ad, &$log)
+    {
+        $log['effect'] = [
+            'campaign' => $campaign->name,
+            'ad_group_id' => $ad->ad_group_id,
+            'ad_id' => $ad->ad_id
+        ];
     }
 }
--- app/Utils/RuleActions/PauseContents.php
+++ app/Utils/RuleActions/PauseContents.php
@@ -9,15 +9,30 @@ use App\Endpoints\GeminiAPI;
 
 class PauseContents extends Root
 {
-    public function process($campaign, $ad)
+    public function process($campaign, $ad, &$log)
     {
+        $log['effect'] = [
+            'campaign' => $campaign->name,
+            'ad_group_id' => $ad->ad_group_id,
+            'ad_id' => $ad->ad_id
+        ];
+
         try {
             $adVendorClass = 'App\\Utils\\AdVendors\\' . ucfirst($campaign->provider->slug);
-
             (new $adVendorClass)->adStatus($campaign, $ad->ad_group_id, $ad->ad_id, Campaign::STATUS_PAUSED);
+            $log['effect']['paused'] = true;
             echo 'Ad was being paused', "\n";
         } catch (Exception $e) {
             echo "Ad wasn't being paused\n";
         }
+    }
+
+    public function visual($campaign, $ad, &$log)
+    {
+        $log['effect'] = [
+            'campaign' => $campaign->name,
+            'ad_group_id' => $ad->ad_group_id,
+            'ad_id' => $ad->ad_id
+        ];
     }
 }
