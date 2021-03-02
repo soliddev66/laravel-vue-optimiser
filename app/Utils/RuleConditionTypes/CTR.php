b5806e695a50424b70b2fe76dfb81173de56a5c4
Nam Huỳnh <namhuynh@MacBook-Pro-cua-Nam.local>
2021-03-02 16:32:29 +0700

    Fixing division by 0 CTR.

--- app/Utils/RuleConditionTypes/CTR.php
+++ app/Utils/RuleConditionTypes/CTR.php
@@ -10,7 +10,12 @@ class CTR extends Root
     {
         $sum_clicks = ReportData::sum($campaign, $redtrack_data, 'lp_clicks');
         $sum_views = ReportData::sum($campaign, $redtrack_data, 'lp_views');
+        if ($sum_views) {
+            $ctr = $sum_clicks / $sum_views * 100;
+        } else {
+            $ctr = INF;
+        }
 
-        return parent::compare($sum_clicks / $sum_views * 100, $rule_condition->amount, $rule_condition->operation);
+        return parent::compare($ctr, $rule_condition->amount, $rule_condition->operation);
     }
 }
