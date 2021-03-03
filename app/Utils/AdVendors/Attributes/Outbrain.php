9dfd5431ed7211ffa56493db4a754d4cc960bce7
Tình Phong <tinhphong007@gmail.com>
2021-03-03 16:45:08 +0700

    Rule Action: Implement get data attribute

--- app/Utils/AdVendors/Attributes/Outbrain.php
+++ app/Utils/AdVendors/Attributes/Outbrain.php
@@ -11,9 +11,12 @@ trait Outbrain
                 return json_decode($data['data'])->summary->impressions;
             case 2:
             default:
-                return $data['impressions'];
+                if (isset($data['impressions'])) {
+                    return $data['impressions'];
+                }
         }
 
+        throw new Exception('No attribute was found.');
     }
 
     public function spend($data, $calculation_type)
@@ -23,8 +26,12 @@ trait Outbrain
                 return json_decode($data['data'])->summary->spend;
             case 2:
             default:
-                return $data['cost'];
+                if (isset($data['cost'])) {
+                    return $data['cost'];
+                }
         }
+
+        throw new Exception('No attribute was found.');
     }
 
     public function clicks($data, $calculation_type)
@@ -34,37 +41,65 @@ trait Outbrain
                 return json_decode($data['data'])->summary->clicks;
             case 2:
             default:
-                return $data['clicks'];
+                if (isset($data['cost'])) {
+                    return $data['clicks'];
+                }
         }
+
+        throw new Exception('No attribute was found.');
     }
 
     public function lpClicks($data, $calculation_type)
     {
+        if (isset($data['lp_clicks'])) {
+            return $data['lp_clicks'];
+        }
+
         throw new Exception('No attribute was found.');
     }
 
     public function lpViews($data, $calculation_type)
     {
+        if (isset($data['lp_views'])) {
+            return $data['lp_views'];
+        }
+
         throw new Exception('No attribute was found.');
     }
 
     public function revenue($data, $calculation_type)
     {
+        if (isset($data['revenue'])) {
+            return $data['revenue'];
+        }
+
         throw new Exception('No attribute was found.');
     }
 
     public function profit($data, $calculation_type)
     {
+        if (isset($data['profit'])) {
+            return $data['profit'];
+        }
+
         throw new Exception('No attribute was found.');
     }
 
     public function cost($data, $calculation_type)
     {
+        if (isset($data['cost'])) {
+            return $data['cost'];
+        }
+
         throw new Exception('No attribute was found.');
     }
 
     public function conversions($data, $calculation_type)
     {
+        if (isset($data['conversions'])) {
+            return $data['conversions'];
+        }
+
         throw new Exception('No attribute was found.');
     }
 }
