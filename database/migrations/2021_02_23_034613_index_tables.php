f06961c2c7b5fd12e1b6527a6b2b5fa4bd4cbb92
Nam Huỳnh <namhuynh@MacBook-Pro-cua-Nam.local>
2021-03-05 11:24:31 +0700

    Release 1.1.5-h2

--- database/migrations/2021_02_23_034613_index_tables.php
+++ database/migrations/2021_02_23_034613_index_tables.php
@@ -13,51 +13,55 @@ class IndexTables extends Migration
      */
     public function up()
     {
-        // Index campaigns table
-        // https://drive.google.com/file/d/1-FXDX02Q1MSw9H2ryIWqbRR-dxW-7_Fd/view?usp=sharing
-        DB::statement('CREATE INDEX campaigns_index_0 ON campaigns (provider_id)');
-        DB::statement('CREATE INDEX campaigns_index_1 ON campaigns (open_id)');
-        DB::statement('CREATE INDEX campaigns_index_2 ON campaigns (advertiser_id)');
-        DB::statement('CREATE INDEX campaigns_index_3 ON campaigns (campaign_id)');
+        try {
+            // Index campaigns table
+            // https://drive.google.com/file/d/1-FXDX02Q1MSw9H2ryIWqbRR-dxW-7_Fd/view?usp=sharing
+            DB::statement('CREATE INDEX campaigns_index_0 ON campaigns (provider_id)');
+            DB::statement('CREATE INDEX campaigns_index_1 ON campaigns (open_id)');
+            DB::statement('CREATE INDEX campaigns_index_2 ON campaigns (advertiser_id)');
+            DB::statement('CREATE INDEX campaigns_index_3 ON campaigns (campaign_id)');
 
-        // Index ad_groups table
-        // https://drive.google.com/file/d/10l6CrMYjgIg4TcsXkVxGfWe4vaub3_D6/view?usp=sharing
-        DB::statement('CREATE INDEX ad_groups_index_0 ON ad_groups (provider_id)');
-        DB::statement('CREATE INDEX ad_groups_index_1 ON ad_groups (campaign_id)');
-        DB::statement('CREATE INDEX ad_groups_index_2 ON ad_groups (open_id)');
-        DB::statement('CREATE INDEX ad_groups_index_3 ON ad_groups (advertiser_id)');
-        DB::statement('CREATE INDEX ad_groups_index_4 ON ad_groups (ad_group_id)');
+            // Index ad_groups table
+            // https://drive.google.com/file/d/10l6CrMYjgIg4TcsXkVxGfWe4vaub3_D6/view?usp=sharing
+            DB::statement('CREATE INDEX ad_groups_index_0 ON ad_groups (provider_id)');
+            DB::statement('CREATE INDEX ad_groups_index_1 ON ad_groups (campaign_id)');
+            DB::statement('CREATE INDEX ad_groups_index_2 ON ad_groups (open_id)');
+            DB::statement('CREATE INDEX ad_groups_index_3 ON ad_groups (advertiser_id)');
+            DB::statement('CREATE INDEX ad_groups_index_4 ON ad_groups (ad_group_id)');
 
-        // Index ads table
-        // https://drive.google.com/file/d/13kNw6WHX9fhWUwRm5wwPhUEwspAFyy-d/view?usp=sharing
-        DB::statement('CREATE INDEX ads_index_0 ON ads (provider_id)');
-        DB::statement('CREATE INDEX ads_index_1 ON ads (campaign_id)');
-        DB::statement('CREATE INDEX ads_index_2 ON ads (open_id)');
-        DB::statement('CREATE INDEX ads_index_3 ON ads (advertiser_id)');
-        DB::statement('CREATE INDEX ads_index_4 ON ads (ad_group_id)');
-        DB::statement('CREATE INDEX ads_index_5 ON ads (ad_id)');
+            // Index ads table
+            // https://drive.google.com/file/d/13kNw6WHX9fhWUwRm5wwPhUEwspAFyy-d/view?usp=sharing
+            DB::statement('CREATE INDEX ads_index_0 ON ads (provider_id)');
+            DB::statement('CREATE INDEX ads_index_1 ON ads (campaign_id)');
+            DB::statement('CREATE INDEX ads_index_2 ON ads (open_id)');
+            DB::statement('CREATE INDEX ads_index_3 ON ads (advertiser_id)');
+            DB::statement('CREATE INDEX ads_index_4 ON ads (ad_group_id)');
+            DB::statement('CREATE INDEX ads_index_5 ON ads (ad_id)');
 
-        // Index redtrack_reports table
-        // https://drive.google.com/file/d/17104vsmCLazgCv2Gy047co4Rt-oyFphQ/view?usp=sharing
-        DB::statement('CREATE INDEX redtrack_reports_index_0 ON redtrack_reports (user_id)');
-        DB::statement('CREATE INDEX redtrack_reports_index_1 ON redtrack_reports (provider_id)');
-        DB::statement('CREATE INDEX redtrack_reports_index_2 ON redtrack_reports (open_id)');
-        DB::statement('CREATE INDEX redtrack_reports_index_3 ON redtrack_reports (advertiser_id)');
-        DB::statement('CREATE INDEX redtrack_reports_index_4 ON redtrack_reports (campaign_id)');
-        DB::statement('CREATE INDEX redtrack_reports_index_5 ON redtrack_reports (sub3)');
-        DB::statement('CREATE INDEX redtrack_reports_index_6 ON redtrack_reports (sub5)');
-        DB::statement('CREATE INDEX redtrack_reports_index_7 ON redtrack_reports (sub6)');
-        DB::statement('CREATE INDEX redtrack_reports_index_8 ON redtrack_reports (date)');
+            // Index redtrack_reports table
+            // https://drive.google.com/file/d/17104vsmCLazgCv2Gy047co4Rt-oyFphQ/view?usp=sharing
+            DB::statement('CREATE INDEX redtrack_reports_index_0 ON redtrack_reports (user_id)');
+            DB::statement('CREATE INDEX redtrack_reports_index_1 ON redtrack_reports (provider_id)');
+            DB::statement('CREATE INDEX redtrack_reports_index_2 ON redtrack_reports (open_id)');
+            DB::statement('CREATE INDEX redtrack_reports_index_3 ON redtrack_reports (advertiser_id)');
+            DB::statement('CREATE INDEX redtrack_reports_index_4 ON redtrack_reports (campaign_id)');
+            DB::statement('CREATE INDEX redtrack_reports_index_5 ON redtrack_reports (sub3)');
+            DB::statement('CREATE INDEX redtrack_reports_index_6 ON redtrack_reports (sub5)');
+            DB::statement('CREATE INDEX redtrack_reports_index_7 ON redtrack_reports (sub6)');
+            DB::statement('CREATE INDEX redtrack_reports_index_8 ON redtrack_reports (date)');
 
-        // Index redtrack_domain_stats table
-        // https://drive.google.com/file/d/1qvdPPheMhL8t98EDewfZU8pIeiM3JqBR/view?usp=sharing
-        DB::statement('CREATE INDEX redtrack_domain_stats_index_0 ON redtrack_domain_stats (user_id)');
-        DB::statement('CREATE INDEX redtrack_domain_stats_index_1 ON redtrack_domain_stats (provider_id)');
-        DB::statement('CREATE INDEX redtrack_domain_stats_index_2 ON redtrack_domain_stats (open_id)');
-        DB::statement('CREATE INDEX redtrack_domain_stats_index_3 ON redtrack_domain_stats (advertiser_id)');
-        DB::statement('CREATE INDEX redtrack_domain_stats_index_4 ON redtrack_domain_stats (campaign_id)');
-        DB::statement('CREATE INDEX redtrack_domain_stats_index_5 ON redtrack_domain_stats (sub1)');
-        DB::statement('CREATE INDEX redtrack_domain_stats_index_6 ON redtrack_domain_stats (date)');
+            // Index redtrack_domain_stats table
+            // https://drive.google.com/file/d/1qvdPPheMhL8t98EDewfZU8pIeiM3JqBR/view?usp=sharing
+            DB::statement('CREATE INDEX redtrack_domain_stats_index_0 ON redtrack_domain_stats (user_id)');
+            DB::statement('CREATE INDEX redtrack_domain_stats_index_1 ON redtrack_domain_stats (provider_id)');
+            DB::statement('CREATE INDEX redtrack_domain_stats_index_2 ON redtrack_domain_stats (open_id)');
+            DB::statement('CREATE INDEX redtrack_domain_stats_index_3 ON redtrack_domain_stats (advertiser_id)');
+            DB::statement('CREATE INDEX redtrack_domain_stats_index_4 ON redtrack_domain_stats (campaign_id)');
+            DB::statement('CREATE INDEX redtrack_domain_stats_index_5 ON redtrack_domain_stats (sub1)');
+            DB::statement('CREATE INDEX redtrack_domain_stats_index_6 ON redtrack_domain_stats (date)');
+        } catch (\Exception $e) {
+            //
+        }
 
         // Update index to use partitions
         DB::statement('
