0761a81d1bb09cbcce50131b88366f12a4b0ba92
Tình Phong <tinhphong007@gmail.com>
2021-03-23 18:13:37 +0700

    Move existing data

--- database/migrations/2021_03_22_111532_remove_columns_rules_table.php
+++ database/migrations/2021_03_22_111532_remove_columns_rules_table.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Models\Rule;
+use App\Models\RuleRuleAction;
+
 class RemoveColumnsRulesTable extends Migration
 {
     /**
@@ -13,6 +16,16 @@ class RemoveColumnsRulesTable extends Migration
      */
     public function up()
     {
+        $rules = Rule::all();
+
+        foreach ($rules as $rule) {
+            (new RuleRuleAction([
+                'rule_id' => $rule->id,
+                'rule_action_id' => $rule->rule_action_id,
+                'action_data' => $rule->action_data
+            ]))->save();
+        }
+
         Schema::table('rules', function (Blueprint $table) {
             $table->dropColumn('rule_action_id');
             $table->dropColumn('action_data');
