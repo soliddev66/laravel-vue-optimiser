c91feb3d1b985934a54362b355df2833e2519d33
Tình Phong <tinhphong007@gmail.com>
2021-04-06 21:25:24 +0700

    Add table index

--- database/migrations/2021_04_06_085003_add_table_indexes.php
+++ database/migrations/2021_04_06_085003_add_table_indexes.php
@@ -168,6 +168,93 @@ class AddTableIndexes extends Migration
             $table->index('week', 'gemini_product_ads_week__index');
             $table->index('day', 'gemini_product_ads_day__index');
         });
+
+        Schema::table('gemini_search_stats', function($table) {
+            $table->index('advertiser_id', 'gemini_search_stats_advertiser_id__index');
+            $table->index('campaign_id', 'gemini_search_stats_campaign_id__index');
+            $table->index('ad_group_id', 'gemini_search_stats_ad_group_id__index');
+            $table->index('ad_id', 'gemini_search_stats_ad_id__index');
+            $table->index('keyword_id', 'gemini_search_stats_keyword_id__index');
+            $table->index('delivered_match_type', 'gemini_search_stats_delivered_match_type__index');
+            $table->index('search_term', 'gemini_search_stats_search_term__index');
+            $table->index('device_type', 'gemini_search_stats_device_type__index');
+            $table->index('day', 'gemini_search_stats_day__index');
+        });
+
+        Schema::table('gemini_site_performance_stats', function($table) {
+            $table->index('advertiser_id', 'gemini_site_performance_stats_advertiser_id__index');
+            $table->index('campaign_id', 'gemini_site_performance_stats_campaign_id__index');
+            $table->index('ad_group_id', 'gemini_site_performance_stats_ad_group_id__index');
+            $table->index('day', 'gemini_site_performance_stats_day__index');
+            $table->index('device_type', 'gemini_site_performance_stats_device_type__index');
+        });
+
+        Schema::table('gemini_slot_performance_stats', function($table) {
+            $table->index('advertiser_id', 'gemini_slot_performance_stats_advertiser_id__index');
+            $table->index('campaign_id', 'gemini_slot_performance_stats_campaign_id__index');
+            $table->index('ad_group_id', 'gemini_slot_performance_stats_ad_group_id__index');
+            $table->index('ad_id', 'gemini_slot_performance_stats_ad_id__index');
+            $table->index('month', 'gemini_slot_performance_stats_month__index');
+            $table->index('week', 'gemini_slot_performance_stats_week__index');
+            $table->index('day', 'gemini_slot_performance_stats_day__index');
+            $table->index('hour', 'gemini_slot_performance_stats_hour__index');
+            $table->index('pricing_type', 'gemini_slot_performance_stats_pricing_type__index');
+            $table->index('source', 'gemini_slot_performance_stats_source__index');
+            $table->index('card_id', 'gemini_slot_performance_stats_card_id__index');
+            $table->index('card_position', 'gemini_slot_performance_stats_card_position__index');
+            $table->index('rendered_type', 'gemini_slot_performance_stats_rendered_type__index');
+        });
+
+        Schema::table('gemini_structured_snippet_extension_stats', function($table) {
+            $table->index('advertiser_id', 'g_s_s_e_stats_advertiser_id__index');
+            $table->index('campaign_id', 'g_s_s_e_stats_campaign_id__index');
+            $table->index('ad_group_id', 'g_s_s_e_stats_ad_group_id__index');
+            $table->index('ad_id', 'g_s_s_e_stats_ad_id__index');
+            $table->index('month', 'g_s_s_e_stats_month__index');
+            $table->index('week', 'g_s_s_e_stats_week__index');
+            $table->index('day', 'g_s_s_e_stats_day__index');
+            $table->index('keyword_id', 'g_s_s_e_stats_keyword_id__index');
+            $table->index('structured_snippet_extn_id', 'g_s_s_e_stats_structured_snippet_extn_id__index');
+            $table->index('pricing_type', 'g_s_s_e_stats_pricing_type__index');
+            $table->index('source', 'g_s_s_e_stats_source__index');
+        });
+
+        Schema::table('gemini_user_stats', function($table) {
+            $table->index('advertiser_id', 'gemini_user_stats_advertiser_id__index');
+            $table->index('campaign_id', 'gemini_user_stats_campaign_id__index');
+            $table->index('audience_id', 'gemini_user_stats_audience_id__index');
+            $table->index('audience_name', 'gemini_user_stats_audience_name__index');
+            $table->index('audience_type', 'gemini_user_stats_audience_type__index');
+            $table->index('audience_status', 'gemini_user_stats_audience_status__index');
+            $table->index('ad_group_id', 'gemini_user_stats_ad_group_id__index');
+            $table->index('day', 'gemini_user_stats_day__index');
+            $table->index('pricing_type', 'gemini_user_stats_pricing_type__index');
+            $table->index('source_name', 'gemini_user_stats_source_name__index');
+            $table->index('gender', 'gemini_user_stats_gender__index');
+            $table->index('device_type', 'gemini_user_stats_device_type__index');
+            $table->index('country', 'gemini_user_stats_country__index');
+            $table->index('state', 'gemini_user_stats_state__index');
+            $table->index('city', 'gemini_user_stats_city__index');
+            $table->index('zip', 'gemini_user_stats_zip__index');
+            $table->index('dma_woeid', 'gemini_user_stats_dma_woeid__index');
+            $table->index('city_woeid', 'gemini_user_stats_city_woeid__index');
+            $table->index('state_woeid', 'gemini_user_stats_state_woeid__index');
+            $table->index('zip_woeid', 'gemini_user_stats_zip_woeid__index');
+            $table->index('country_woeid', 'gemini_user_stats_country_woeid__index');
+            $table->index('location_type', 'gemini_user_stats_location_type__index');
+        });
+
+        Schema::table('image_tags', function($table) {
+            $table->index('image_id', 'image_tags_image_id__index');
+            $table->index('tag_id', 'image_tags_tag_id__index');
+        });
+
+        Schema::table('job_batches', function($table) {
+            $table->index('name', 'job_batches_name__index');
+        });
+
+
+
     }
 
     /**
@@ -331,6 +418,81 @@ class AddTableIndexes extends Migration
             $table->dropIndex('gemini_product_ads_month__index');
             $table->dropIndex('gemini_product_ads_week__index');
             $table->dropIndex('gemini_product_ads_day__index');
+        });
+
+        Schema::table('gemini_search_stats', function($table) {
+            $table->dropIndex('gemini_search_stats_advertiser_id__index');
+            $table->dropIndex('gemini_search_stats_campaign_id__index');
+            $table->dropIndex('gemini_search_stats_ad_group_id__index');
+            $table->dropIndex('gemini_search_stats_ad_id__index');
+            $table->dropIndex('gemini_search_stats_keyword_id__index');
+            $table->dropIndex('gemini_search_stats_delivered_match_type__index');
+            $table->dropIndex('gemini_search_stats_search_term__index');
+            $table->dropIndex('gemini_search_stats_device_type__index');
+            $table->dropIndex('gemini_search_stats_day__index');
+        });
+
+        Schema::table('gemini_site_performance_stats', function($table) {
+            $table->dropIndex('gemini_site_performance_stats_advertiser_id__index');
+            $table->dropIndex('gemini_site_performance_stats_campaign_id__index');
+            $table->dropIndex('gemini_site_performance_stats_ad_group_id__index');
+            $table->dropIndex('gemini_site_performance_stats_day__index');
+            $table->dropIndex('gemini_site_performance_stats_device_type__index');
+        });
+
+        Schema::table('gemini_slot_performance_stats', function($table) {
+            $table->dropIndex('gemini_slot_performance_stats_advertiser_id__index');
+            $table->dropIndex('gemini_slot_performance_stats_campaign_id__index');
+            $table->dropIndex('gemini_slot_performance_stats_ad_group_id__index');
+            $table->dropIndex('gemini_slot_performance_stats_ad_id__index');
+            $table->dropIndex('gemini_slot_performance_stats_month__index');
+            $table->dropIndex('gemini_slot_performance_stats_week__index');
+            $table->dropIndex('gemini_slot_performance_stats_day__index');
+            $table->dropIndex('gemini_slot_performance_stats_hour__index');
+            $table->dropIndex('gemini_slot_performance_stats_pricing_type__index');
+            $table->dropIndex('gemini_slot_performance_stats_source__index');
+            $table->dropIndex('gemini_slot_performance_stats_card_id__index');
+            $table->dropIndex('gemini_slot_performance_stats_card_position__index');
+            $table->dropIndex('gemini_slot_performance_stats_rendered_type__index');
+        });
+
+        Schema::table('gemini_structured_snippet_extension_stats', function($table) {
+            $table->dropIndex('g_s_s_e_stats_advertiser_id__index');
+            $table->dropIndex('g_s_s_e_stats_campaign_id__index');
+            $table->dropIndex('g_s_s_e_stats_ad_group_id__index');
+            $table->dropIndex('g_s_s_e_stats_ad_id__index');
+            $table->dropIndex('g_s_s_e_stats_month__index');
+            $table->dropIndex('g_s_s_e_stats_week__index');
+            $table->dropIndex('g_s_s_e_stats_day__index');
+            $table->dropIndex('g_s_s_e_stats_keyword_id__index');
+            $table->dropIndex('g_s_s_e_stats_structured_snippet_extn_id__index');
+            $table->dropIndex('g_s_s_e_stats_pricing_type__index');
+            $table->dropIndex('g_s_s_e_stats_source__index');
+        });
+
+        Schema::table('gemini_user_stats', function($table) {
+            $table->dropIndex('gemini_user_stats_advertiser_id__index');
+            $table->dropIndex('gemini_user_stats_campaign_id__index');
+            $table->dropIndex('gemini_user_stats_audience_id__index');
+            $table->dropIndex('gemini_user_stats_audience_name__index');
+            $table->dropIndex('gemini_user_stats_audience_type__index');
+            $table->dropIndex('gemini_user_stats_audience_status__index');
+            $table->dropIndex('gemini_user_stats_ad_group_id__index');
+            $table->dropIndex('gemini_user_stats_day__index');
+            $table->dropIndex('gemini_user_stats_pricing_type__index');
+            $table->dropIndex('gemini_user_stats_source_name__index');
+            $table->dropIndex('gemini_user_stats_gender__index');
+            $table->dropIndex('gemini_user_stats_device_type__index');
+            $table->dropIndex('gemini_user_stats_country__index');
+            $table->dropIndex('gemini_user_stats_state__index');
+            $table->dropIndex('gemini_user_stats_city__index');
+            $table->dropIndex('gemini_user_stats_zip__index');
+            $table->dropIndex('gemini_user_stats_dma_woeid__index');
+            $table->dropIndex('gemini_user_stats_city_woeid__index');
+            $table->dropIndex('gemini_user_stats_state_woeid__index');
+            $table->dropIndex('gemini_user_stats_zip_woeid__index');
+            $table->dropIndex('gemini_user_stats_country_woeid__index');
+            $table->dropIndex('gemini_user_stats_location_type__index');
         });
     }
 }
