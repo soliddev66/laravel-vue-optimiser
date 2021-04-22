82828d2adfa35f677791c4fbefc7fa19e795a83a
Tình Phong <tinhphong007@gmail.com>
2021-04-22 18:36:52 +0700

    Creative Sets: Add set for Yahoo

---
+++ resources/js/components/CreativeSetSets.vue
@@ -0,0 +1,99 @@
+<template>
+  <section>
+    <div class="vld-parent">
+      <loading :active.sync="isLoading" :can-cancel="true" :is-full-page="fullPage"></loading>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col">
+        <div class="card">
+          <div class="card-body table-responsive">
+            <table ref="creativeSetSetsTable" id="creativeSetSetsTable" class="table table-bordered table-hover text-center">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Name</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="item in data" :key="item.id">
+                  <td>{{ item.id }}</td>
+                  <td>{{ item.name }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import _ from 'lodash';
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+
+export default {
+  props: {
+    type: {
+      type: String,
+      default: 'media'
+    },
+    adType: {
+      type: String,
+      default: 'image'
+    }
+  },
+  components: {
+    Loading
+  },
+
+  watch: {
+    type: function (newValue) {
+      console.log(newValue)
+    },
+    adType: function (newValue) {
+      console.log(newValue)
+    },
+  },
+
+  mounted() {
+    console.log('Component mounted.')
+    this.getData().then(() => {
+      $('#creativeSetSetsTable').DataTable({
+        retrieve: true,
+        paging: true,
+        ordering: true,
+        info: true,
+        stateSave: false,
+        autoWidth: false,
+        pageLength: 10,
+      });
+    })
+  },
+  data() {
+    return {
+      data: [],
+      errors: {},
+      isLoading: false,
+      fullPage: true
+    }
+  },
+  methods: {
+    getData() {
+      this.isLoading = true;
+      return axios.get('/creatives/data')
+        .then(response => {
+          this.data = response.data.creativeSets
+        })
+        .catch(error => {
+          this.errors = error.response.data
+        })
+        .finally(() => {
+          this.isLoading = false;
+        });
+    },
+
+  }
+}
+</script>
