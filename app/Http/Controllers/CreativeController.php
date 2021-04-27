452a8f4d7ecbc00fc5ec1f393f1db60b33d2027b
Tình Phong <tinhphong007@gmail.com>
2021-04-27 17:57:08 +0700

    Creative Sets: Integrate Tinify"

--- app/Http/Controllers/CreativeController.php
+++ app/Http/Controllers/CreativeController.php
@@ -8,6 +8,7 @@ use App\Models\VideoSet;
 use App\Models\TitleSet;
 
 use DB;
+use Storage;
 use Illuminate\Support\Facades\Gate;
 
 class CreativeController extends Controller
@@ -38,6 +39,42 @@ class CreativeController extends Controller
 
                 $imageSet->image = $set['image'];
                 $imageSet->hq_image = $set['hqImage'];
+
+                if (!Storage::disk('images')->exists('creatives/800x800/' . md5(auth()->id()))) {
+                    Storage::disk('images')->makeDirectory('creatives/800x800/' . md5(auth()->id()));
+                }
+
+                if (!Storage::disk('images')->exists('creatives/1200x627/' . md5(auth()->id()))) {
+                    Storage::disk('images')->makeDirectory('creatives/1200x627/' . md5(auth()->id()));
+                }
+
+                if (!Storage::disk('images')->exists('creatives/1200x628/' . md5(auth()->id()))) {
+                    Storage::disk('images')->makeDirectory('creatives/1200x628/' . md5(auth()->id()));
+                }
+
+                $source = \Tinify\fromFile(storage_path('app/public/images/') . $set['hqImage']);
+
+                $resized = $source->resize([
+                    'method' => 'cover',
+                    'width' => 800,
+                    'height' => 800
+                ]);
+                $resized->toFile(storage_path('app/public/images/creatives/800x800/') . $set['hqImage']);
+
+                $resized = $source->resize([
+                    'method' => 'cover',
+                    'width' => 1200,
+                    'height' => 627
+                ]);
+                $resized->toFile(storage_path('app/public/images/creatives/1200x627/') . $set['hqImage']);
+
+                $resized = $source->resize([
+                    'method' => 'cover',
+                    'width' => 1200,
+                    'height' => 628
+                ]);
+                $resized->toFile(storage_path('app/public/images/creatives/1200x628/') . $set['hqImage']);
+
                 $imageSet->save();
 
                 $creativeSet->imageSets()->save($imageSet);
@@ -126,7 +163,8 @@ class CreativeController extends Controller
             'creativeSets' => 'required|present|array',
             'creativeSets.*.image' => 'required_if:creativeSetType,image',
             'creativeSets.*.hqImage' => 'required_if:creativeSetType,image',
-            'creativeSets.*.image' => 'required_if:creativeSetType,video',
+            'creativeSets.*.portraitImage' => 'required_if:creativeSetType,video',
+            'creativeSets.*.landscapeImage' => 'required_if:creativeSetType,video',
             'creativeSets.*.video' => 'required_if:creativeSetType,video',
             'creativeSets.*.title' => 'required_if:creativeSetType,title',
         ]);
