6b3da3ef89c6a33687d835431c77a2b3a70e6b89
Tình Phong <tinhphong007@gmail.com>
2021-05-20 14:21:36 +0700

    Currency converter

--- app/Console/Commands/UpdateCurrencyCommand_gemini_slot_performance_stats.php
+++ app/Console/Commands/UpdateCurrencyCommand_gemini_slot_performance_stats.php
@@ -6,14 +6,14 @@ use Illuminate\Console\Command;
 
 use DB;
 
-class UpdateCurrencyCommand_gemini_performance_stats extends Command
+class UpdateCurrencyCommand_gemini_slot_performance_stats extends Command
 {
     /**
      * The name and signature of the console command.
      *
      * @var string
      */
-    protected $signature = 'currency:gemini_performance_stats';
+    protected $signature = 'currency:gemini_slot_performance_stats';
 
     /**
      * The console command description.
@@ -40,9 +40,9 @@ class UpdateCurrencyCommand_gemini_performance_stats extends Command
     public function handle()
     {
         DB::transaction(function () {
-            DB::update('UPDATE gemini_performance_stats SET spend = spend * 0.13, max_bid = max_bid * 0.13, ad_extn_spend = ad_extn_spend * 0.13, average_cpc = average_cpc * 0.13, average_cpm = average_cpm * 0.13, cost_per_video_view = cost_per_video_view * 0.13');
+            DB::update('UPDATE gemini_slot_performance_stats SET average_cpc = average_cpc * 0.13, spend = spend * 0.13');
 
-            file_put_contents('public/gemini_performance_stats', 'SUCCESS');
+            file_put_contents('public/gemini_slot_performance_stats', 'SUCCESS');
 
             $this->info('The command was successful!');
         });
